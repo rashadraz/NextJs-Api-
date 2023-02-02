1161e13636eefd3b4ed211e49bdafa4b24ef377f
rashad <akyrashad@gmail.com>
2023-02-02 08:44:27 +0530

    mapping and api cal

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,20 +1,39 @@
-function handler(req,res){
-    if(req.method == 'POST'){
-       const email =  req.body.email;
-       const feedback =  req.body.feedback;
+import fs from "fs";
+import path from "path";
 
-       const newFeedback = {
-        id: new Date().toISOString(),
-        email:email,
-        text:feedback
+function buildFeedbackpath() {
+	return path.join(process.cwd(), "data", "feedback.json");
+}
+function extractFeedback(filePath) {
+	const fileData = fs.readFileSync(filePath);
+	const data = JSON.parse(fileData);
+	return data;
+}
 
-       };
+function handler(req, res) {
+	if (req.method === "POST") {
+		const email = req.body.email;
+		const feedbackText = req.body.text;
 
-       //store that in a database or in a file
-    }
-    res.status(200).json({
-        message: "This Works!!!!!!!!"
-    });
+		const newFeedback = {
+			id: new Date().toISOString(),
+			email: email,
+			text: feedbackText,
+		};
+
+		//store that in a database or in a file
+		const filePath = buildFeedbackpath();
+		const data = extractFeedback(filePath);
+		data.push(newFeedback);
+		fs.writeFileSync(filePath, JSON.stringify(data));
+		res.status(201).json({ message: "Success!", feedback: newFeedback });
+	} else {
+		const filePath = buildFeedbackpath();
+        const data = extractFeedback(filePath);
+		res.status(200).json({
+			feedback:data,
+		});
+	}
 }
 
 export default handler;
